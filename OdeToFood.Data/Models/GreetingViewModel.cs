4f500b514c706481bb00ed981829cf063faec0ea
Joe Bhullar <joebhullar11@gmail.com>
2021-09-21 23:36:13 -0600

    Added In Memory Database

---
+++ OdeToFood.Data/Models/GreetingViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data.Models
+{
+    class GreetingViewModel
+    {
+        public string Message { get; set; } // 2:28
+
+    }
+}
---
+++ OdeToFood.Web/Controllers/GreetingController.cs
@@ -0,0 +1,21 @@
+﻿using OdeToFood.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+
+namespace OdeToFood.Web.Controllers
+{
+    public class GreetingController : Controller
+    {
+        // GET: Greeting
+        public ActionResult Index()
+        {
+            var model = new GreetingViewModel();
+            model.Message = ConfigurationManager.AppSettings["message"];
+            return View(model);
+        }
+    }
+}
